492498ca5d707ff2beac8a384720b41b0032d268
Tushar Shaw <145246175+TUSHAR-SHAWw@users.noreply.github.com>
2024-02-26 16:55:28 +0530

    Initial commit

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p15m&d=g!28#ha%nhk189qkfxtq*#u+i#zf8i1+zv82w4)2r8w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
