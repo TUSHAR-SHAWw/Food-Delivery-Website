8b7a2be446e48c55e0f0c28186a14f373b830051
Tushar Shaw <145246175+TUSHAR-SHAWw@users.noreply.github.com>
2024-02-27 18:30:34 +0530

    Initial commit

--- static/js/script.js
+++ static/js/script.js
@@ -21,3 +21,17 @@ function updateCounter(foodId, quantity,cost,cart_total) {
       $(".cart_total").text('Cart Total Rs : '+cart_total );
 
     }
+function convertToicon(category) {
+  let list = {
+    'Fish':'<i class="fa-solid fa-fish-fins fa-xl"></i>  Fish',
+    'Rice':'<i class="fa-solid fa-bowl-rice fa-xl" ></i>  Rice',
+    'Chicken':'<i class="fa-solid fa-dove fa-xl" ></i> Chicken',
+    'Mutton':'<i class="fa-solid fa-drumstick-bite fa-xl"></i>  Mutton',
+    'Veg':'<i class="fa-solid fa-leaf fa-xl" ></i> Veg',
+    'Paratha':'<i class="fa-solid fa-pizza-slice fa-xl"></i>  Paratha',
+    'Non-veg':'<i class="fa-solid fa-bone fa-xl"></i></i>  Non-veg'
+  };
+  let icon = list[category]
+  return icon;
+}
+'<i class="fa-regular fa-star"></i>'
